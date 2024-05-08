f8a26a3a8901623272f93e1ff04449171e279a3a
Diwakar Shukla <diwakar.shukla@paytm.com>
2024-05-08 13:14:45 +0530

    LC26

---
+++ lc/src/array/LC80RemoveDups2.java
@@ -0,0 +1,36 @@
+package array;
+
+import java.util.Arrays;
+import java.util.TreeMap;
+
+public class LC80RemoveDups2 {
+    public static void main(String[] args) {
+        int arr[] = new int[]{0, 0, 1, 1, 1, 1, 2, 3, 3};
+        LC80RemoveDups2 lc80RemoveDups2 = new LC80RemoveDups2();
+        lc80RemoveDups2.removeDuplicates(arr);
+    }
+
+
+    public int removeDuplicates(int[] nums) {
+        int op = 0;
+        int prev = Integer.MIN_VALUE;
+        int prevCount = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i == 0) {
+                prev = nums[i];
+                nums[op++] = prev;
+                prevCount++;
+            } else if (prev == nums[i] && prevCount < 2) {
+                nums[op++] = prev;
+                prevCount++;
+            } else {
+                prevCount = 0;
+                prev = nums[i];
+                nums[op++] = prev;
+                prevCount++;
+            }
+        }
+        return op;
+    }
+}
