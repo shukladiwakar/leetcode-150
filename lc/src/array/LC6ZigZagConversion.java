80364560b3cff3e45956f1b92fb3feabd37d18b8
Diwakar Shukla <csediwakar@gmail.com>
2024-05-27 13:29:38 +0530

    local commit
    Signed-off-by: Diwakar Shukla <csediwakar@gmail.com>

---
+++ lc/src/array/LC6ZigZagConversion.java
@@ -0,0 +1,13 @@
+package array;
+
+public class LC6ZigZagConversion {
+    public static void main(String[] args) {
+
+    }
+
+    /*
+            P	0	0	0	A	0	0	0	H	0	0	0	N
+            0	A	0	P	0	L	0	S	0	I	0	I	0	G
+            0	0	Y	0	0	0	I	0	0	0	R	0	0
+     */
+}
