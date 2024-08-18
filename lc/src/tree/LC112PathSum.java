b1649bcba535396be5f0673126c2cecb08dd80f0
Diwakar Shukla <diwakar.shukla@paytm.com>
2024-08-18 22:40:00 +0530

    LC26

--- lc/src/tree/LC112PathSum.java
+++ lc/src/tree/LC112PathSum.java
@@ -1,9 +1,9 @@
 package tree;
 
+import leetcodeds.ListNode;
 import leetcodeds.TreeNode;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class LC112PathSum {
     public boolean hasPathSum(TreeNode root, int targetSum) {
@@ -57,7 +57,50 @@ public class LC112PathSum {
     //    public int findSecondMinimumValue(TreeNode root) {
 //
 //    }
-    public List<Integer> rightSideView(TreeNode root) {
+//    public List<Integer> rightSideView(TreeNode root) {
+//
+//    }
 
+    //LC6 ZigZag Conversion
+    public static String convert(String s, int numRows) {
+        int arr[][] = new int[numRows][13];
+
+        for (int row[] : arr) {
+            Arrays.fill(row, 0);
+        }
+
+        System.out.println(Arrays.deepToString(arr));
+        return "";
+    }
+
+    public static void main(String[] args) {
+        convert("diwakar", 3);
+    }
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        List<Integer> list3 = new ArrayList<>();
+        while (list1 != null) {
+            list3.add(list1.val);
+            list1 = list1.next;
+        }
+
+        while (list2 != null) {
+            list3.add(list2.val);
+            list2 = list2.next;
+        }
+
+        Collections.sort(list3);
+        if (list3.size() == 0) {
+            return new ListNode();
+        }
+        System.out.println(list3.size());
+        ListNode op = new ListNode(list3.get(0), null);
+
+        for (int i = 1; i < list3.size(); i++) {
+            ListNode temp = new ListNode(list3.get(i), null);
+            op.next = temp;
+        }
+
+        return op;
     }
 }
